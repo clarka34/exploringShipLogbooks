df6c2f291400065f5f6d22e92fbc5a62f21ed67a
Emma Cotter <ecotter@uw.edu>
2016-02-26 17:54:52 +0000

    efficiency increases to word count functions using counters

--- exploring-ship-logbooks/word_count.py
+++ exploring-ship-logbooks/word_count.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import math
+from collections import Counter
 
 def count_key_words(data, columns, key_words):
     """
@@ -23,64 +23,43 @@ def count_key_words(data, columns, key_words):
 
     """
 
-    # initialize word count list
-    word_count_list = []
-
-    # intitialize dictionary for total word count
-    word_count_total = {}
-    for key_word in key_words:
-        word_count_total[key_word] = 0
+    all_words = []
 
     # for every row in data
     for index, row in data.iterrows():
 
         # aggregate words from each specified column
-        all_words = []
         for column in columns:
             remark = row[column]
-            try:
-                math.isnan(remark)
-            except:
-                all_words += remark.split(' ')
-
-        # find number of instances of each keyword in this entry
-        word_count_dict = {}
-        for key_word in key_words:
-            matches = [s for s in all_words if key_word.upper() in str(s)]
-            word_count_dict[key_word] = len(matches)
-            word_count_total[key_word] += len(matches)
-
-        # append word count to list
-        word_count_list.append(word_count_dict)
-
-    return word_count_list, word_count_total
-
-def count_all_words(data, columns):
-    """returns a dataframe containing a count of all words in the logbooks (columns in pandas dataframe
-    data)
-    """
-    # intitialize dictionary for total word count
-    word_count_dict = {}
-
-    # for every row in data
-    for index, row in data.iterrows():
-
-        # aggregate words from each specified column
-        all_words = []
-        for column in columns:
-            remark = row[column]
-
             if isinstance(remark,str):
                 all_words += remark.split(' ')
 
-        for word in all_words:
-            try:
-                word_count_dict[word] += 1
-            except:
-                word_count_dict[word] = 1
+    # count each key word
+    key_word_count = {}
+    for key_word in key_words:
+        key_word_count[key_word] = all_words.count(key_word.upper())
 
-        if index == 10:
-            break
+    return key_word_count
+
+from collections import Counter
+
+def count_all_words(data, columns):
+    """returns a dataframe containing a count of all words in the logbooks
+    (columns in pandas dataframe data)
+    """
+
+    all_words = []
+
+    # for every row in data
+    for index, row in data.iterrows():
+
+        # aggregate words from each specified column
+        for column in columns:
+            remark = row[column]
+            if isinstance(remark,str):
+                all_words += remark.split(' ')
+
+    word_count_dict = Counter(all_words)
 
     word_count_pd = pd.DataFrame(data=list(word_count_dict.items()),columns=['Word','Count'])
     word_count_pd = word_count_pd.sort(columns = 'Count', ascending = False)
