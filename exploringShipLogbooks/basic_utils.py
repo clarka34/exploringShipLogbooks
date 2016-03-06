650c874be687700e66d034be33f8226485528f28
Alicia Clark <clarka34@uw.edu>
2016-03-05 23:05:50 -0800

    Final one hot encoder notebook and changes made to basic_utils.py

--- exploringShipLogbooks/basic_utils.py
+++ exploringShipLogbooks/basic_utils.py
@@ -1,6 +1,7 @@
 # Import modules needed for the following functions
 import zipfile
 
+import numpy as np
 import pandas as pd
 import ipywidgets as widgets
 
@@ -50,6 +51,27 @@ def isolate_columns(df, desired_columns):
 
     return df
 
+def remove_undesired_columns(df, desired_columns):
+    """
+    Removes undesired columns from the data set
+    Inputs:
+    df -- dataframe containing logbook data
+    desired_columns -- columns that user wants to user
+    -- formatted as w.value (where w is the output of creating_widget)
+    Ouput:
+    unwanted_columns -- columns that the user does not want
+    """
+
+    # Initializes list of all columns and then removes the desired columns
+    # from the list
+
+    undesired_columns = list(df.columns.values)
+
+    for value in desired_columns:
+        undesired_columns.remove(value)
+
+    return undesired_columns
+
 
 def create_widget(df):
     """
@@ -78,22 +100,74 @@ def create_widget(df):
 
     return w
 
+def clean_data(df):
+    """
+    Makes all the data lower case and also strips the white space from the end
+    of each entry
+
+    Input: df -- dataframe containing the logbook data
+
+    Ouput: df -- dataframe containing the cleaned data
+    """
+
+    df['VoyageFrom'] = df['VoyageFrom'].astype(str).map(lambda x: x.lower().rstrip())
+    df['VoyageTo'] = df['VoyageTo'].astype(str).map(lambda x: x.lower().rstrip())
+    df['ShipName'] = df['ShipName'].astype(str).map(lambda x: x.lower().rstrip())
+    df['ShipType'] = df['ShipType'].astype(str).map(lambda x: x.lower().rstrip())
+    df['Company'] = df['Company'].astype(str).map(lambda x: x.lower().rstrip())
+    df['Nationality'] = df['Nationality'].astype(str).map(lambda x: x.lower().rstrip())
+    return df
+
 def label_encoder(column):
+    """
+    Converts categorical data to numerical data
+    """
     return LabelEncoder().fit_transform(column)
 
+def label_encoder_key(column):
+    """
+    Key for the conversion of categorical data to numerical data
+    """
+    return LabelEncoder().fit(column).classes_
+
 def one_hot_encoder(column):
+    """
+    Converts numerical data to one hot encoded data
+    """
     return OneHotEncoder().fit_transform(label_encoder(column).reshape(-1,1)).toarray()
 
 def encode_data(df):
     '''
-    Transforms all columns of the dataframe specified using
-    LabelEncoder() and OneHotEncoder().
+    Transforms all columns of the dataframe specified using LabelEncoder() and
+    OneHotEncoder().
+
+    Input: df -- dataframe containing the logbook data
+
+    Ouput: encoded_data -- dataframe containing the one hot encoded data
+           encoder -- keys for the one hot encoded data
     '''
     encoded_data = []
-
+    encoder = []
     # Encodes only the specified columns in the function call
     for col in df.columns:
         encoded_data.append(one_hot_encoder(df[col]))
+        encoder.append(label_encoder_key(df[col]))
 
     encoded_data = np.hstack(encoded_data)
-    return encoded_data
+    encoder = np.hstack(encoder)
+    return encoded_data, encoder
+
+def encode_data_df(df):
+    '''
+    Creates a pandas dataframe of the encoded date with each column labelled
+
+    Input: df -- dataframe containing the logbook data
+
+    Output: encoded_df -- dataframe containing the encoded data
+    '''
+
+    encoded_data, encoder = encode_data(df)
+
+    encoded_df = pd.DataFrame(encoded_data, columns = encoder)
+
+    return encoded_df
