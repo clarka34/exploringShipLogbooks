96b81cfe79bd428c9fd1e6431f8cfa4657b3baae
Emma Cotter <ecotter@uw.edu>
2016-03-12 15:32:18 -0800

    added predict and fit functions. all functions tested up to encoder

--- exploringShipLogbooks/classification.py
+++ exploringShipLogbooks/classification.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from . import wordCount as wc
+from . import wordcount as wc
 from sklearn import preprocessing
 from sklearn.naive_bayes import MultinomialNB
 from sklearn import tree
@@ -78,7 +78,7 @@ class LogbookClassifier:
 
         # sort by logbookIdent and set as index
         self.cliwoc_data = self.cliwoc_data.sort_values('LogbookIdent', ascending=True)
-        self.cliwoc_data_all = self.cliwoc_data.copy.set_index('LogbookIdent', drop=False).copy()
+        self.cliwoc_data_all = self.cliwoc_data.set_index('LogbookIdent', drop=False).copy()
         self.cliwoc_data = self.cliwoc_data.set_index('LogbookIdent', drop = False)
         self.cliwoc_data = self.cliwoc_data.drop_duplicates('LogbookIdent')
 
@@ -109,6 +109,8 @@ class LogbookClassifier:
         self.all_data = pd.concat([self.cliwoc_data, self.slave_voyage_logs], ignore_index=True)
         self.all_data = clean_data(self.all_data)
 
+        del self.cliwoc_data, self.slave_voyage_logs
+
     def match_similar_words(self):
 
         fuzz_columns = ['Nationality', 'ShipType', 'VoyageFrom', 'VoyageTo']
@@ -122,20 +124,84 @@ class LogbookClassifier:
         self.all_data['no_data'] = self.all_data['nan'].apply(lambda x: x.any(), axis=1).astype(int)
         self.all_data = self.all_data.drop('nan', axis=1)
 
-    def fit_classifier(self, indexes):
-        # indexes will be 1 and 3 with the current code (negative and positive
-        # training data, respectively)
+    def extract_data_sets(self, multiplier = True):
+        # extract logs to classify later
+        self.unclassified_logs = self.all_data[self.all_data['slave_logs']==0]
 
-        classifier.fit(training_data[::,1::], classes)
-        # convert to np array
-        # join all masks and classes (i.e. pos and neg)
-        # add what columns
-        # fit data
-        pass
+        # extract first validation data set
+        self.validation_set_1 = self.all_data[self.all_data['slave_logs']==2]
+
+        # reserve first 20% of slave_voyage_logs as validation set 2
+        validation_set_2_indices = range(slave_voyage_indices.min(),
+                                         slave_voyage_indices.min() + round(len(slave_voyage_indices)*.2))
+        self.validation_set_2 = self.all_data.iloc[validation_set_2_indices]
+
+        # extract training data for positive and negative
+        training_logs_pos = self.all_data.drop(validation_set_2_indices)
+        training_logs_pos = training_logs_pos[training_logs_pos['slave_logs']==3]
+
+        training_logs_neg = self.all_data[self.all_data['slave_logs']==1]
+
+        # calculate multiplier to make data sets equal size
+        if multiplier:
+            repeat_multiplier = round(len(training_logs_pos)/len(training_logs_neg))
+        else:
+            repeat_multiplier = 1
+
+        # create list of classes for training data (0 is for non-slave, 1 is for slave)
+        # index matches training_data
+        training_classes = np.zeros(len(training_logs_neg)).repeat(repeat_multiplier)
+        #classes = np.append(classes, np.ones(len(training_logs_pos)))
+        self.training_classes = np.append(training_classes, np.ones(len(training_logs_pos)))
+
+        # join training data
+        neg_rep = pd.concat([training_logs_neg]*repeat_multiplier)
+        self.training_data = pd.concat([neg_rep, training_logs_pos], ignore_index=True)
+
+        del all_data
+
+    def fit_classifier(self):
+        # convert to numpy array
+        columns = list(training_data.columns)
+        columns.remove('slave_logs')
+        training_data_to_fit = self.training_data.as_matrix(columns)
+
+        self.classifier.fit(training_data_to_fit[::,1::], self.training_classes)
+
+    def validate_classifier(self):
+            validation_sets = [self.validation_set_1, self.validation_set_2]
+
+            for i, validation_set in enumerate(validation_sets):
+                columns = list(validation_set.columns)
+                columns.remove('slave_logs')
+                validation_set_matrix = validation_set.as_matrix(columns)
+                predictions = classifier.predict(validation_set_matrix[::,1::])
+
+                counts = collections.Counter(predictions)
+                percent_correct = (counts[expected_class]/(len(predictions))* 100)
+
+                print('Validation set', i, ' was classified as', expected_class,
+                      round(percent_correct,2), '% of the time')
+
+    def classify(self):
+
+        data_to_classify = self.unclassified_logs.copy()
 
 
-    def validate_classifier(self, masks, desired_vals):
-        pass
+        # convert to numpy and classify
+        columns = list(data_to_classify.columns)
+        columns.remove('slave_logs')
+        data_matrix = data_to_classify.as_matrix(columns)
+        predictions = classifier.predict(data_matrix[::,1::])
 
-    def classify(self, masks):
-        pass
+        # revalue slave_log ID column to indicate classification
+        data_to_classify['slave_logs'] = predictions + 4
+
+        # print statstics
+        counts = collections.Counter(predictions)
+
+        for key in counts:
+            percent = (counts[key]/(len(predictions))* 100)
+            print(round(percent, 2), 'of data was classified as ', key)
+
+        self.unclassified_logs = data_to_classify
